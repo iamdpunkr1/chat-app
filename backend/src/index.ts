ec924faa32b279d7cdd069f1a58740626e0f15c1
Dipankar Prasad <65856604+iamdpunkr1@users.noreply.github.com>
2024-03-07 17:49:06 +0530

    port changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -229,8 +229,8 @@ io.on("connection", (socket: Socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+server.listen(5001, () => {
+  console.log("Server is running on port 5001");
 });
 
 // // import Redis from 'ioredis';
