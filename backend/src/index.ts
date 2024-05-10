51b7216113c713384f0985d0075402777db90127
Dipankar Prasad <iamdpunkr@gmail.com>
2024-05-10 18:16:39 +0530

    chore: Update server port to use HTTPS for alegralabs.com

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -59,7 +59,7 @@ console.log = function() {
   process.stdout.write(`${new Date().toISOString()} - ${Array.from(arguments).join(' ')}\n`);
 };
 
-const PORT = "http://localhost:5173" // "https://www.alegralabs.com"
+const PORT =  "https://www.alegralabs.com"
 const FILEURL = process.env.FILE_URL;
 console.log("FILEURL: ", FILEURL);
 
